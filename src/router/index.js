39c5a70414f3bfae8a9e4913190a612444b39f49
Ayush Rasailee <rasaileeayush17@gmail.com>
2025-01-21 15:16:09 +0545

    component

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Signin from '@/views/Signin.vue'
 import Signup from '@/views/Signup.vue'
-import FutsalAvailability from '@/views/FutsalAvailability.vue'
+import FutsalAvailability from '@/components/FutsalAvailability.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
